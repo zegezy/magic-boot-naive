d6b695d818e724d132340f0ffccb6e09d966569e
Yean <clarence@lightwing.net>
2023-03-17 16:01:54 +0800

    <fix>样式美化,增加右键菜单

--- src/layout/tabs.vue
+++ src/layout/tabs.vue
@@ -1,22 +1,57 @@
 <template>
-  <n-space>
+<!--  <n-space>-->
+  <div>
     <n-tag
+      @contextmenu="handleContextMenu(tab,$event)"
       v-for="tab in tabsStore.getTabs"
-      closable
+      :closable="tab.path!==`/home`"
       @close="handleClose(tab.path)"
       @click="jump(tab)"
-      :type="tabsStore.getCurrentTab == tab.path ? 'success' : 'default'"
+      :type="tabsStore.getCurrentTab == tab.path ? 'primary' : 'default'"
     >
       {{ tab.meta.title }}
     </n-tag>
-  </n-space>
+
+    <n-dropdown
+        placement="bottom-start"
+        trigger="manual"
+        :x="xAxis"
+        :y="yAxis"
+        :options="dropdownOptions"
+        :show="showDropdown"
+        :on-clickoutside="onClickoutside"
+        @select="handleDropdownSelect"
+    />
+  </div>
+<!--  </n-space>-->
 </template>
 
 <script setup>
   import { useTabsStore } from '@/store/modules/tabsStore'
   import router from '@/scripts/router'
+  import {ref} from "vue";
   const tabsStore = useTabsStore()
   const tabs = tabsStore.getTabs
+
+  const showDropdown = ref(false);
+  const dropdownOptions = ref([
+    {
+      label:"关闭当前",
+      key:0
+    },
+    {
+      label:"关闭其它",
+      key:1
+    },
+    {
+      label:"关闭全部",
+      key:2
+    }
+  ])
+
+  // 弹出菜单展示位置
+  const xAxis = ref(0);
+  const yAxis = ref(0);
 
   function handleClose(path){
     if(tabs.length == 1){
@@ -42,11 +77,33 @@
       query: tabs.filter(it => it.path == item.path)[0].query
     })
   }
+
+  function handleContextMenu(item,e){
+    e.preventDefault();
+    xAxis.value = e.clientX;
+    yAxis.value = e.clientY;
+    showDropdown.value = true;
+  }
+
+  function onClickoutside(){
+    showDropdown.value = false;
+  }
+
+  function handleDropdownSelect(e){
+    //TODO 处理弹出菜单点击事件
+    console.log("点击项:",e)
+    showDropdown.value = false;
+  }
 </script>
 
 <style scoped>
+
 .n-tag{
+  padding: 18px;
   cursor: pointer;
-  height: 32px;
+  height: 40px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  margin-right:4px;
 }
 </style>
