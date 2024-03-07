9d7181831f3344d398a9c8c2d88c0baa61bb3047
zegezy <1098696801@qq.com>
2024-03-07 20:35:51 +0800

    导入"系统功能"所用到的包

--- src/scripts/compiler/magic-import.js
+++ src/scripts/compiler/magic-import.js
@@ -2,12 +2,24 @@ import * as vue from "vue";
 import * as NaiveUI from 'naive-ui';
 import * as router from '@/scripts/router'
 import xicons from '@/scripts/xicons'
+import svgIcons from '@/scripts/svg-icons'
+import * as dictStore from "@/store/modules/dictStore";
+import * as userStore from "@/store/modules/userStore";
+import MbIcon from '@/components/magic/basic/mb-icon.vue';
+import * as lodashEs from 'lodash-es'
+import * as vueRouter from 'vue-router'
 
 const libs = {
     vue,
     'naive-ui': NaiveUI,
     '@/scripts/xicons': xicons,
-    '@/scripts/router': router
+    '@/scripts/svg-icons': svgIcons,
+    '@/scripts/router': router,
+    '@/store/modules/dictStore': dictStore,
+    '@/store/modules/userStore': userStore,
+    '@/components/magic/basic/mb-icon.vue': MbIcon,
+    'lodash-es': lodashEs,
+    'vue-router': vueRouter
 }
 
 window.___magic__import__ = function(lib, name){
