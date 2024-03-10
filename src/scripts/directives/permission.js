c49f6234b985ea597fc5762df112dad642252d9b
zegezy <1098696801@qq.com>
2024-03-10 23:22:20 +0800

    演示模式显示所有权限

--- src/scripts/directives/permission.js
+++ src/scripts/directives/permission.js
@@ -5,7 +5,7 @@ const permission = {
     mounted(el, binding) {
         if (binding.value) {
             const permissionList = useUserStore().getAuths
-            if (!isEmpty(permissionList) && !permissionList.includes(binding.value)) {
+            if (!isEmpty(permissionList) && !permissionList.includes(binding.value) && import.meta.env.MODE != 'demo') {
                 el.remove()
             }
         }
