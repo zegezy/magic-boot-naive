0036e9ba56d0cf7e865e1c4cfc1551e0a2376ffa
zegezy <1098696801@qq.com>
2024-04-09 21:39:10 +0800

    修复动态组件渲染两次问题

--- src/layout/nested-router.vue
+++ src/layout/nested-router.vue
@@ -2,7 +2,7 @@
     <router-view v-slot="{ Component }">
         <transition name="fade" mode="out-in" appear>
             <keep-alive :include="keepAliveInclude">
-                <component v-if="tabsStore.getShow && !keepaliveIframes.some(it => it.path == $route.path)" :is="Component" :key="$route.path"/>
+                <component v-if="tabsStore.getShow && !keepaliveIframes.some(it => it.path == $route.path) && !keepaliveDynamicComponents.some(it => it.path == $route.path)" :is="Component" :key="$route.path"/>
             </keep-alive>
         </transition>
     </router-view>
@@ -13,5 +13,8 @@ import {useTabsStore} from '@/store/modules/tabsStore'
 import {computed} from "vue";
 const tabsStore = useTabsStore()
 const keepAliveInclude = computed(() => tabsStore.getTabs.filter(it => it.meta.keepAlive && !it.meta.componentName).map(it => it.path.substring(it.path.lastIndexOf('/') + 1)))
+// 单独处理 "iframe" 并且开启缓存的页面
 const keepaliveIframes = computed(() => tabsStore.getTabs.filter(it => $common.filterIframeTabs(it)))
+// 缓存“动态组件”
+const keepaliveDynamicComponents = computed(() => tabsStore.getTabs.filter(it => it.meta.componentName))
 </script>
